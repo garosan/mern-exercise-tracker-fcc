7a79ea0c4e3b30baca58e1c473d9f87f3e399298
Garo San <garosanchez27@gmail.com>
2020-07-13 08:33:17 -0500

    Connect MongoDB

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully.");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
